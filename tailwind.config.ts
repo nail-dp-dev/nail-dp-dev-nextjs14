bbfb6664d1d6708d80a141d82a83ddec7082da0f
이승현 <lsh746527@gmail.com>
2024-06-13 14:48:17 +0900

    conf : colors 설정 #6

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -17,14 +17,71 @@ const config: Config = {
       },
     },
     colors: {
-      '':'',
+      // Original
+      white: "#FFFFFF",
+      black: "#000000",
+
+      // Theme
+      themeDark: "#121212",
+
+      // Text
+      textBlack: "#141217",
+      textDarkPurple: "#756982",
+      textLightYellow: "#F9F3E7",
+      textKakao: "#191919",
+      textGoogle: "#1F1F1F",
+
+      // Social bg
+      kakaoYellow: "#FEE500",
+      naverGreen: "#03C75A",
+      googleGray: "#F2F2F5",
+
+      // Gray 1
+      lightGray: "#F2F2F5",
+      inputLightGray: "#F2F2F5",
+      menuLightGray: "#F2F2F5",
+      buttonLightGray: "#F2F2F5",
+
+      // Gray 2
+      hashTagGray: "#F2F0F5",
+
+      // Gray 3
+      darkGray: "#E0DEE3",
+      buttonDarkGray: "#E0DEE3",
+      navBotSolidGray: "#E0DEE3",
+
+      // Gray 4
+      navMenuBotSolidGray: "#E5E8EB",
+
+      // Gray 5
+      addFolderGray: "#D9D9D9",
+
+      // Gray 6
+      postInputGray: "#E0DBE5",
+
+      // Purple 1
+      purple: "#B98CE0",
+
+      // Purple 2
+      darkPurple: "#756982",
+
+      // Red
+      red: "#FF007A",
+
+      // Orange
+      orange: "#FFAC30",
+      searchOrange: "#FFAC30",
+
+      // Yellow
+      lightYellow: "#F9F3E7",
     },
+
     screens: {
-      'ph': [{'min': '360px', 'max': '580px'},],
-      'tb': [{'min': '581px', 'max': '1279px'},],
-      'lt': [{'min': '1280px', 'max': '1838px'},],
-      'dt': [{ 'min': '1839px' }],
-    } 
+      ph: [{ min: "360px", max: "580px" }],
+      tb: [{ min: "581px", max: "1279px" }],
+      lt: [{ min: "1280px", max: "1838px" }],
+      dt: [{ min: "1839px" }],
+    },
   },
   plugins: [],
 };
