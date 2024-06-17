2cae84277ede393d970741862ba4ecbb9acff446
이승현 <lsh746527@gmail.com>
2024-06-17 16:40:08 +0900

    conf : redux 설치 후 example로 text 완료 #26

--- app/(arhcive)/page.tsx
+++ app/(arhcive)/page.tsx
@@ -1,12 +1,34 @@
-'use client'
+"use client";
 
-import DownloadIcon from '../../public/assets/svg/download.svg'
+import DownloadIcon from "../../public/assets/svg/download.svg";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "../../lib/store";
+import { decrement, increment } from "../../lib/features/example/exampleSlice";
 
 export default function Home() {
+  const reduxTest = useSelector((state: RootState) => state.example.value);
+  const dispatch = useDispatch();
+
   return (
     <main className="w-[100%] h-[100%] flex flex-col">
       Home page 입니다.
       <DownloadIcon fill="#04fa9a" />
+      {/* Redux 상태 테스트 */}
+      <div>Hello world! {reduxTest}</div>
+      <div>
+        <button
+          className="px-4 py-2 text-black"
+          onClick={() => dispatch(increment())}
+        >
+          +
+        </button>
+        <button
+          className="px-4 py-2 text-black "
+          onClick={() => dispatch(decrement())}
+        >
+          -
+        </button>
+      </div>
     </main>
   );
 }
--- app/test/small-test/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client'
-
-export default function SmallTestComponent() {
-  return (
-    <div className="w-[100%] h-[100%] flex flex-col">
-      Small-Test component 입니다.
-    </div>
-  );
-}
