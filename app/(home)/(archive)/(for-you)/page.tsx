4dca82ef6f5d7b6704e99fe0999715e90c19efab
LKW9 <sg4582@naver.com>
2024-06-19 17:54:18 +0900

    refactor : 하트아이콘 사이즈 변경 기능 추가 #34

--- app/(home)/(archive)/(for-you)/page.tsx
+++ app/(home)/(archive)/(for-you)/page.tsx
@@ -1,11 +1,11 @@
-import HeartButton from '../../../../components/buttons/HeartButton';
+import HeartButton from '../../../../components/animations/HeartButton';
 
 export default function ForYouPage() {
 
   return (
     <section className="ForYouContainer w-full h-full">
       ForYouContainer 입니다.
-      <HeartButton/>
+      <HeartButton width='21' height='19'/>
     </section>
   );
 }
--- components/animations/PlusButton.tsx
+++ components/animations/PlusButton.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 
 export default function PlusButton() {
   const [isClick, setIsClick] = useState(false);
