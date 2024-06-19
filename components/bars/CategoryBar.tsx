f891d44c19d00426fa21c8e66a3d5e4c8c77fa92
Ju-jh <jjdong9108@gmail.com>
2024-06-19 13:59:07 +0900

    refactor : menubar and menubutton svg component error 관련 fix

--- components/bars/CategoryBar.tsx
+++ components/bars/CategoryBar.tsx
@@ -1,19 +1,27 @@
-import Link from 'next/link';
-import { archiveCategoryElements } from '../../constants';
+'use client'
 
-export default function CategoryBar() {
+import Link from 'next/link';
+import { CategoryBarProps } from '../../constants/interface';
+import { usePathname } from 'next/navigation';
+
+
+export default function CategoryBar({elements}: CategoryBarProps) {
+
+  const path = usePathname()
+
   return (
-    <div className='categoryBar w-full h-[66px] flex flex-col items-center justify-center'>
-      <div className='innerBar w-full h-[53px] flex gap-[32px]'>
-        <Link href={'/'} className='inline-flex h-[100%] items-center justify-center border-purple border-b-[3px]'>
-          <p className='text-[14px] font-[700]'>For You</p>
-        </Link>
-        <Link href={'/'} className='inline-flex h-[100%] items-center justify-center border-purple border-b-[3px]'>
-          <p className='text-[14px] font-[700]'>For You</p>
-        </Link>
-        <Link href={'/'} className='inline-flex h-[100%] items-center justify-center border-purple border-b-[3px]'>
-          <p className='text-[14px] font-[700]'>For You</p>
-        </Link>
+    <div className='categoryBar w-full h-[66px] flex items-center justify-between'>
+      <div className='categoryDiv w-full h-[53px] flex gap-[32px]'>
+        {
+          elements.map((item, index)=>(
+            <Link key={index} href={item.url} className={`inline-flex h-[100%] transition-all items-center justify-center ${path === item.url ? 'border-purple' : 'border-navMenuBotSolidGray' }  border-b-[3px]`}>
+              <p className='text-[14px] font-[700]'>{item.name}</p>
+            </Link>
+          ))
+        }
+      </div>
+      <div className=''>
+        dfw
       </div>
     </div>
   )
--- components/buttons/MenuButton.tsx
+++ components/buttons/MenuButton.tsx
@@ -1,22 +1,62 @@
+'use client'
+
+import HomeIcon from '../../public/assets/svg/home.svg'
+import MyArchiveIcon from '../../public/assets/svg/my-archive.svg'
+import DesignIcon from '../../public/assets/svg/design.svg'
+import ReservationIcon from '../../public/assets/svg/reservation.svg'
+import BuyIcon from '../../public/assets/svg/buy.svg'
+import MyPageIcon from '../../public/assets/svg/my-page.svg'
+import SettingIcon from '../../public/assets/svg/setting.svg'
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 
 interface MenuElementsProps{
-  icon:  React.ComponentType<React.SVGProps<SVGSVGElement>>;
+  icon: string;
   url: string,
   name: string,
   desc: string,
   isLast: boolean;
-  path: string;
 }
 
-export default function MenuButton({ icon: Icon, url, name, desc, isLast, path }: MenuElementsProps) {
+export default function MenuButton({ icon , url, name, desc, isLast }: MenuElementsProps) {
+
+  const path = usePathname();
+  console.log(path)
 
   return (
     <div className={`w-full ${!isLast && 'mb-[8px]'} group`}>
       <Link href={url} className={`${path === url ? 'bg-menuLightGray' : ''} w-full h-[40px] flex items-center justify-between py-[12px] px-[8px] rounded-2xl group-hover:bg-purple group-hover:text-white`}>
         <div className='flex items-center'>
           <div className='flex items-center justify-center w-[36px] h-[24px]'>
-            <Icon className={`menuIcon mr-[12px] fill-transparent stroke-black fill-current group-hover:fill-white  group-hover:stroke-white ${path === url && 'fill-black'}`} />
+            {
+              icon === 'HomeIcon' &&
+              <HomeIcon className={`menuIcon mr-[12px] fill-transparent stroke-black fill-current group-hover:fill-white  group-hover:stroke-white ${path === url && 'fill-black'}`} />
+              }
+            {
+              icon === 'MyArchiveIcon' &&
+              <MyArchiveIcon className={`menuIcon mr-[12px] fill-transparent stroke-black fill-current group-hover:fill-white  group-hover:stroke-white ${path === url && 'fill-black'}`} />
+              }
+            {
+              icon === 'DesignIcon' &&
+              <DesignIcon className={`menuIcon mr-[12px] fill-transparent stroke-black fill-current group-hover:fill-white  group-hover:stroke-white ${path === url && 'fill-black'}`} />
+              }
+            {
+              icon === 'ReservationIcon' &&
+              <ReservationIcon className={`menuIcon mr-[12px] fill-transparent stroke-black fill-current group-hover:fill-white  group-hover:stroke-white ${path === url && 'fill-black'}`} />
+              }
+            { 
+              icon === 'BuyIcon' &&
+              <BuyIcon className={`menuIcon mr-[12px] fill-transparent stroke-black fill-current group-hover:fill-white  group-hover:stroke-white ${path === url && 'fill-black'}`} />
+              }
+            {
+              icon === 'MyPageIcon' &&
+              <MyPageIcon className={`menuIcon mr-[12px] fill-transparent stroke-black fill-current group-hover:fill-white  group-hover:stroke-white ${path === url && 'fill-black'}`} />
+              }
+            {
+              icon === 'SettingIcon' &&
+              <SettingIcon className={`menuIcon mr-[12px] fill-transparent stroke-black fill-current group-hover:fill-white  group-hover:stroke-white ${path === url && 'fill-black'}`} />
+            }
           </div>
           <span className={` text-[14px] ${path === url ? 'font-[700]' : 'font-[500]'}`}>{name}</span>
         </div>
