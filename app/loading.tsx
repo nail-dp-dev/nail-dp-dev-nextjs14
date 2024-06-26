e997972d5e35c6899f3ee15938d3d02284121b0f
이승현 <lsh746527@gmail.com>
2024-06-27 03:20:13 +0900

    chore : loading 컴포넌트에 다크테마 추가

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,14 +1,13 @@
-import React from "react";
+import React from 'react';
 
 import NailLogoIcon from '../public/assets/svg/nail-logo.svg';
 
-
 export default function Loading() {
   return (
-    <div className="w-full h-full flex items-center justify-center rounded-2xl z-40" >
+    <div className="w-full h-full bg-white dark:bg-themeDark flex items-center justify-center rounded-2xl z-40">
       <div className="animate-fadeInOutFloat">
         <NailLogoIcon />
       </div>
     </div>
-  )
+  );
 }
