4e8db776fff66dff6a81b247964bfba8339c6b2e
이승현 <lsh746527@gmail.com>
2024-06-26 16:03:00 +0900

    chore : 검색어 임시 이미지 추가 #29

--- components/boxes/SearchWord.tsx
+++ components/boxes/SearchWord.tsx
@@ -9,7 +9,7 @@ export default function SearchWord() {
       {searchWords.map((item, index) => (
         <button
           key={index}
-          className="bg-textDarkPurple p-3 overflow-hidden 
+          className="relative bg-textDarkPurple p-3 overflow-hidden 
           w-full
           sm:w-[calc(50%-10px)]
           md:w-[calc(33.333%-10px)]
@@ -18,10 +18,12 @@ export default function SearchWord() {
           2xl:w-[calc(16.7%-10px)] h-[110px] 
           rounded-2xl flex flex-col justify-center items-center bg-cover bg-center"
           style={{
-            backgroundImage: `url(${item.data.postImageUrl})`,
+            // backgroundImage: `url(${item.data.postImageUrl})`,
+            backgroundImage: `url('https://images.unsplash.com/photo-1522337660859-02fbefca4702?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwzNzAzN3wwfDF8c2VhcmNofDJ8fG5haWx8ZW58MHx8fHwxNjE2NjMxMjM0&ixlib=rb-1.2.1&q=80&w=400')`,
           }}
         >
-          <div className="text-white text-[0.94rem] font-extrabold">
+          <div className="absolute inset-0 bg-black bg-opacity-50 rounded-2xl"></div>
+          <div className="relative text-white text-[0.94rem] font-extrabold">
             {item.data.tags[0].tagName}
           </div>
         </button>
