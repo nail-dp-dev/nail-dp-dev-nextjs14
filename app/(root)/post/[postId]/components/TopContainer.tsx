f91dac40a464cb1f854ec7571e77858c2371164e
이승현 <lsh746527@gmail.com>
2024-07-10 16:42:10 +0900

    feat : 팔로우, 팔로잉 상태 변경 로직 구현 #83

--- app/(root)/post/[postId]/components/TopContainer.tsx
+++ app/(root)/post/[postId]/components/TopContainer.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import UserImage from '../../../../../components/ui/UserImage';
 import UserInfo from '../../../../../components/ui/UserInfo';
 import { PostsDetailData } from '../../../../../types/dataType';
@@ -12,9 +12,21 @@ export default function TopContainer({ user }: userProps) {
     return <div>사용자를 찾을 수 없습니다.</div>;
   }
 
+  const [isFollowing, setIsFollowing] = useState(user.followingStatus);
+  const [followerCount, setFollowerCount] = useState(user.followerCount);
+
+  const handleFollowToggle = () => {
+    if (isFollowing) {
+      setFollowerCount(followerCount - 1);
+    } else {
+      setFollowerCount(followerCount + 1);
+    }
+    setIsFollowing(!isFollowing);
+  };
+
   return (
     <div
-      className="flex  flex-wrap snap-y snap-end
+      className="flex  snap-y snap-end flex-wrap
       items-center justify-between bg-white  p-2"
     >
       <div className="wrap-left flex flex-wrap items-center gap-4 ">
@@ -31,17 +43,20 @@ export default function TopContainer({ user }: userProps) {
             nicknameStyle="text-base font-medium"
             statsStyle="text-14px-normal-dP"
           />
-          <span className="text-14px-normal-dP">
-            {user.followerCount} 팔로워
-          </span>
+          <span className="text-14px-normal-dP">{followerCount} 팔로워</span>
         </div>
 
         <div className="flex flex-wrap gap-2">
-          <button className="button-layout button-color px-[22.5px] py-[5.5px]">
-            팔로우
-          </button>
-          <button className="button-layout border-2 border-darkPurple bg-white px-[22.5px] py-[3.5px] text-darkPurple">
-            팔로잉
+          <button
+            className={`button-layout px-[22.5px] py-[5.5px] 
+            ${
+              isFollowing
+                ? 'border-2 border-darkPurple bg-white text-darkPurple'
+                : 'button-color'
+            }`}
+            onClick={handleFollowToggle}
+          >
+            {isFollowing ? '팔로잉' : '팔로우'}
           </button>
         </div>
       </div>
