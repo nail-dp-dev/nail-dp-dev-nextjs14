69e681e3c6c583df0e4932060d0a8ea0d2272132
LKW9 <sg4582@naver.com>
2024-07-11 17:13:30 +0900

    refactor : 게시글 작성 api 요청 수정 #80

--- api/post/tempPostCreate.ts
+++ api/post/tempPostCreate.ts
@@ -1,23 +1,39 @@
-export const tempPostCreate = async (formData: {
+export const tempPostCreate = async (postData: {
   postContent: string;
   tags: { tagName: string }[];
   tempSave: boolean;
   boundary: string;
-  photos: { mediaFile: FormData }[];
+  photos: File[];
 }) => {
   try {
+    const multipartFormData = new FormData();
+    multipartFormData.append(
+      'content',
+      new Blob(
+        [
+          JSON.stringify({
+            postContent: postData.postContent,
+            tags: postData.tags,
+            tempSave: postData.tempSave,
+            boundary: postData.boundary,
+          }),
+        ],
+        { type: 'application/json' },
+      ),
+    );
+    postData.photos.forEach((file) => {
+      const blob = new Blob([file], { type: file.type });
+      multipartFormData.append('photos', blob, file.name);
+    });
+
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/posts/temp`,
       {
         method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
         credentials: 'include',
-        body: JSON.stringify({ formData }),
+        body: multipartFormData,
       },
     );
-    
   } catch (error) {
     if (error instanceof TypeError) {
       console.error('Network error or invalid JSON:', error);
