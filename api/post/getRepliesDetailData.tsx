a68a02a8422e1120ee2219918537950e7516d7aa
이승현 <lsh746527@gmail.com>
2024-07-12 10:20:37 +0900

    chore : 임시 데이터 수정

--- api/post/getRepliesDetailData.tsx
+++ api/post/getRepliesDetailData.tsx
@@ -9,38 +9,29 @@ export const repliesDetail: ReplyData[] = [
       {
         commentId: 1,
         profileUrl:
-          'https://i.pinimg.com/236x/6a/11/be/6a11be206763cdf800b241ca39edf3c9.jpg',
-        commentUserNickname: 'alice',
+          'https://i.pinimg.com/236x/27/eb/39/27eb393c94e6f1016f1a14263d5e2f79.jpg',
+        commentUserNickname: 'somi',
         commentDate: '20240701',
-        commentContent: '정말 그렇네요!',
+        commentContent: '정말 그렇네요! 네일 색상이 너무 예뻐요.',
         likeCount: 2,
       },
       {
         commentId: 2,
         profileUrl:
-          'https://i.pinimg.com/736x/cc/58/c8/cc58c82cb5b49433fa23701b5fe2ed10.jpg',
-        commentUserNickname: 'bob',
+          'https://i.pinimg.com/236x/6a/11/be/6a11be206763cdf800b241ca39edf3c9.jpg',
+        commentUserNickname: 'jake',
         commentDate: '20240701',
-        commentContent: '동의합니다!',
+        commentContent: '저도 이 디자인 좋아해요. 특별한 날에 딱이에요.',
         likeCount: 1,
       },
       {
-        commentId: 2,
+        commentId: 3,
         profileUrl:
           'https://i.pinimg.com/736x/cc/58/c8/cc58c82cb5b49433fa23701b5fe2ed10.jpg',
-        commentUserNickname: 'bob',
+        commentUserNickname: 'lisa',
         commentDate: '20240701',
-        commentContent: '동의합니다!',
-        likeCount: 1,
-      },
-      {
-        commentId: 2,
-        profileUrl:
-          'https://i.pinimg.com/736x/cc/58/c8/cc58c82cb5b49433fa23701b5fe2ed10.jpg',
-        commentUserNickname: 'bob',
-        commentDate: '20240701',
-        commentContent: '동의합니다!',
-        likeCount: 1,
+        commentContent: '이 네일 디자인 정말 독특하네요!',
+        likeCount: 4,
       },
     ],
   },
@@ -50,13 +41,40 @@ export const repliesDetail: ReplyData[] = [
     commentId: 2,
     data: [
       {
-        commentId: 3,
+        commentId: 4,
         profileUrl:
-          'https://i.pinimg.com/236x/3c/9e/a7/3c9ea7e72fc2c7e8e8f8f8f8f8f8f8f8.jpg',
-        commentUserNickname: 'charlie',
+          'https://i.pinimg.com/236x/29/4d/ff/294dff945ada2d9b991a30088b8be7f9.jpg',
+        commentUserNickname: 'mark',
         commentDate: '20240702',
-        commentContent: '색상이 정말 예뻐요!',
+        commentContent: '이 네일은 어디서 했나요? 정말 멋져요.',
         likeCount: 3,
+      },
+      {
+        commentId: 5,
+        profileUrl:
+          'https://i.pinimg.com/236x/f6/cd/3f/f6cd3fca69bbcd61976c1a9ae89b4ebb.jpg',
+        commentUserNickname: 'nina',
+        commentDate: '20240702',
+        commentContent: '친구들이 다들 예쁘다고 할 것 같아요!',
+        likeCount: 5,
+      },
+      {
+        commentId: 6,
+        profileUrl:
+          'https://i.pinimg.com/236x/36/ac/8a/36ac8afd4d85c8d83c3f4c0608a3aaeb.jpg',
+        commentUserNickname: 'alex',
+        commentDate: '20240702',
+        commentContent: '정말로 아름다운 디자인이에요.',
+        likeCount: 2,
+      },
+      {
+        commentId: 7,
+        profileUrl:
+          'https://i.pinimg.com/236x/21/34/9b/21349b5b26da4645cee2d96a9196acda.jpg',
+        commentUserNickname: 'amy',
+        commentDate: '20240702',
+        commentContent: '정말 멋지네요! 저도 해보고 싶어요.',
+        likeCount: 4,
       },
     ],
   },
@@ -66,22 +84,31 @@ export const repliesDetail: ReplyData[] = [
     commentId: 3,
     data: [
       {
-        commentId: 4,
+        commentId: 8,
         profileUrl:
-          'https://i.pinimg.com/236x/4a/33/df/4a33dfdd06ed3fd9b3ff4a68d3c6d59e.jpg',
-        commentUserNickname: 'david',
+          'https://i.pinimg.com/236x/11/8a/3f/118a3f800a0e81b5a905be226120102c.jpg',
+        commentUserNickname: 'aaron',
         commentDate: '20240703',
-        commentContent: '유용한 정보네요!',
-        likeCount: 2,
+        commentContent: '이런 네일 아트는 처음 봐요! 정말 멋져요.',
+        likeCount: 4,
       },
       {
-        commentId: 5,
+        commentId: 9,
         profileUrl:
-          'https://i.pinimg.com/236x/5a/77/8d/5a778d69e6f5f25d8f9c5a2c0e4efb10.jpg',
-        commentUserNickname: 'emma',
+          'https://i.pinimg.com/236x/90/38/9a/90389ac84d4f156d53515ef04c7c5dab.jpg',
+        commentUserNickname: 'sara',
         commentDate: '20240703',
-        commentContent: '감사합니다!',
-        likeCount: 4,
+        commentContent: '네일 색상이 정말 독특하네요!',
+        likeCount: 3,
+      },
+      {
+        commentId: 10,
+        profileUrl:
+          'https://i.pinimg.com/236x/ab/93/71/ab9371801fc86397b388f8924609cadf.jpg',
+        commentUserNickname: 'steve',
+        commentDate: '20240703',
+        commentContent: '이 네일 디자인 정말 마음에 들어요.',
+        likeCount: 2,
       },
     ],
   },
@@ -91,13 +118,40 @@ export const repliesDetail: ReplyData[] = [
     commentId: 4,
     data: [
       {
-        commentId: 6,
+        commentId: 11,
+        profileUrl:
+          'https://i.pinimg.com/236x/27/eb/39/27eb393c94e6f1016f1a14263d5e2f79.jpg',
+        commentUserNickname: 'somi',
+        commentDate: '20240704',
+        commentContent: '디자인이 정말 독창적이에요!',
+        likeCount: 5,
+      },
+      {
+        commentId: 12,
         profileUrl:
           'https://i.pinimg.com/236x/6a/11/be/6a11be206763cdf800b241ca39edf3c9.jpg',
-        commentUserNickname: 'frank',
+        commentUserNickname: 'jake',
         commentDate: '20240704',
-        commentContent: '멋진 디자인이에요!',
-        likeCount: 5,
+        commentContent: '이 네일은 정말 완벽해요!',
+        likeCount: 4,
+      },
+      {
+        commentId: 13,
+        profileUrl:
+          'https://i.pinimg.com/736x/cc/58/c8/cc58c82cb5b49433fa23701b5fe2ed10.jpg',
+        commentUserNickname: 'lisa',
+        commentDate: '20240704',
+        commentContent: '이 네일 디자인은 정말 예뻐요!',
+        likeCount: 3,
+      },
+      {
+        commentId: 14,
+        profileUrl:
+          'https://i.pinimg.com/236x/29/4d/ff/294dff945ada2d9b991a30088b8be7f9.jpg',
+        commentUserNickname: 'mark',
+        commentDate: '20240704',
+        commentContent: '네일 색상이 정말 예술적이에요!',
+        likeCount: 2,
       },
     ],
   },
@@ -107,12 +161,30 @@ export const repliesDetail: ReplyData[] = [
     commentId: 5,
     data: [
       {
-        commentId: 7,
+        commentId: 15,
         profileUrl:
-          'https://i.pinimg.com/236x/6a/11/be/6a11be206763cdf800b241ca39edf3c9.jpg',
-        commentUserNickname: 'grace',
+          'https://i.pinimg.com/236x/29/4d/ff/294dff945ada2d9b991a30088b8be7f9.jpg',
+        commentUserNickname: 'mark',
         commentDate: '20240705',
-        commentContent: '좋은 글이에요!',
+        commentContent: '정말 대단해요! 이 네일 아트는 최고예요.',
+        likeCount: 2,
+      },
+      {
+        commentId: 16,
+        profileUrl:
+          'https://i.pinimg.com/236x/f6/cd/3f/f6cd3fca69bbcd61976c1a9ae89b4ebb.jpg',
+        commentUserNickname: 'nina',
+        commentDate: '20240705',
+        commentContent: '이 디자인 너무 좋아요!',
+        likeCount: 4,
+      },
+      {
+        commentId: 17,
+        profileUrl:
+          'https://i.pinimg.com/236x/36/ac/8a/36ac8afd4d85c8d83c3f4c0608a3aaeb.jpg',
+        commentUserNickname: 'alex',
+        commentDate: '20240705',
+        commentContent: '완벽한 네일 아트입니다.',
         likeCount: 3,
       },
     ],
@@ -123,12 +195,21 @@ export const repliesDetail: ReplyData[] = [
     commentId: 6,
     data: [
       {
-        commentId: 8,
+        commentId: 18,
         profileUrl:
-          'https://i.pinimg.com/236x/6a/11/be/6a11be206763cdf800b241ca39edf3c9.jpg',
-        commentUserNickname: 'henry',
+          'https://i.pinimg.com/236x/21/34/9b/21349b5b26da4645cee2d96a9196acda.jpg',
+        commentUserNickname: 'amy',
         commentDate: '20240706',
-        commentContent: '정말 멋져요!',
+        commentContent: '정말 세련된 디자인이네요!',
+        likeCount: 5,
+      },
+      {
+        commentId: 19,
+        profileUrl:
+          'https://i.pinimg.com/236x/11/8a/3f/118a3f800a0e81b5a905be226120102c.jpg',
+        commentUserNickname: 'aaron',
+        commentDate: '20240706',
+        commentContent: '이 네일은 정말 예뻐요!',
         likeCount: 4,
       },
     ],
@@ -139,13 +220,22 @@ export const repliesDetail: ReplyData[] = [
     commentId: 7,
     data: [
       {
-        commentId: 9,
+        commentId: 20,
         profileUrl:
-          'https://i.pinimg.com/236x/6a/11/be/6a11be206763cdf800b241ca39edf3c9.jpg',
-        commentUserNickname: 'irene',
+          'https://i.pinimg.com/236x/90/38/9a/90389ac84d4f156d53515ef04c7c5dab.jpg',
+        commentUserNickname: 'sara',
         commentDate: '20240707',
-        commentContent: '감사합니다!',
+        commentContent: '네일 색상이 너무 좋아요!',
         likeCount: 5,
+      },
+      {
+        commentId: 21,
+        profileUrl:
+          'https://i.pinimg.com/236x/ab/93/71/ab9371801fc86397b388f8924609cadf.jpg',
+        commentUserNickname: 'steve',
+        commentDate: '20240707',
+        commentContent: '이 네일 아트 정말 대단해요!',
+        likeCount: 3,
       },
     ],
   },
@@ -155,13 +245,22 @@ export const repliesDetail: ReplyData[] = [
     commentId: 8,
     data: [
       {
-        commentId: 10,
+        commentId: 22,
+        profileUrl:
+          'https://i.pinimg.com/236x/27/eb/39/27eb393c94e6f1016f1a14263d5e2f79.jpg',
+        commentUserNickname: 'somi',
+        commentDate: '20240708',
+        commentContent: '이 디자인 정말 좋아요!',
+        likeCount: 2,
+      },
+      {
+        commentId: 23,
         profileUrl:
           'https://i.pinimg.com/236x/6a/11/be/6a11be206763cdf800b241ca39edf3c9.jpg',
-        commentUserNickname: 'jack',
+        commentUserNickname: 'jake',
         commentDate: '20240708',
-        commentContent: '잘 봤어요!',
-        likeCount: 2,
+        commentContent: '이 네일 아트 정말 독특하네요!',
+        likeCount: 3,
       },
     ],
   },
@@ -171,12 +270,21 @@ export const repliesDetail: ReplyData[] = [
     commentId: 9,
     data: [
       {
-        commentId: 11,
+        commentId: 24,
         profileUrl:
-          'https://i.pinimg.com/236x/6a/11/be/6a11be206763cdf800b241ca39edf3c9.jpg',
-        commentUserNickname: 'kate',
+          'https://i.pinimg.com/736x/cc/58/c8/cc58c82cb5b49433fa23701b5fe2ed10.jpg',
+        commentUserNickname: 'lisa',
         commentDate: '20240709',
-        commentContent: '좋은 정보 감사합니다!',
+        commentContent: '정말 예뻐요!',
+        likeCount: 3,
+      },
+      {
+        commentId: 25,
+        profileUrl:
+          'https://i.pinimg.com/236x/29/4d/ff/294dff945ada2d9b991a30088b8be7f9.jpg',
+        commentUserNickname: 'mark',
+        commentDate: '20240709',
+        commentContent: '정말 멋져요!',
         likeCount: 4,
       },
     ],
@@ -187,12 +295,21 @@ export const repliesDetail: ReplyData[] = [
     commentId: 10,
     data: [
       {
-        commentId: 12,
+        commentId: 26,
         profileUrl:
-          'https://i.pinimg.com/236x/6a/11/be/6a11be206763cdf800b241ca39edf3c9.jpg',
-        commentUserNickname: 'leo',
+          'https://i.pinimg.com/236x/90/38/9a/90389ac84d4f156d53515ef04c7c5dab.jpg',
+        commentUserNickname: 'sara',
         commentDate: '20240710',
-        commentContent: '멋져요!',
+        commentContent: '정말 마음에 들어요!',
+        likeCount: 2,
+      },
+      {
+        commentId: 27,
+        profileUrl:
+          'https://i.pinimg.com/236x/f6/cd/3f/f6cd3fca69bbcd61976c1a9ae89b4ebb.jpg',
+        commentUserNickname: 'nina',
+        commentDate: '20240710',
+        commentContent: '이 네일 디자인 정말 훌륭해요!',
         likeCount: 3,
       },
     ],
