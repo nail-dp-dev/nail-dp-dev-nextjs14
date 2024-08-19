b4bca79aee4b3079c4b1e999b08c6fe67cc04b35
이승현 <lsh746527@gmail.com>
2024-08-19 14:57:18 +0900

    chore : 중복 함수 제거

--- api/post/postLikeComment.ts
+++ api/post/postLikeComment.ts
@@ -15,21 +15,3 @@ export const postLikeComment = async (postId: number, commentId: number) => {
   console.log('Comment liked successfully');
   return response.json();
 };
-
-export const deleteUnlikeComment = async (postId: number, commentId: number) => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/posts/${postId}/comment/${commentId}/like`, {
-    method: 'DELETE',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    credentials: 'include',
-  });
-
-  if (!response.ok) {
-    console.error('Failed to unlike comment');
-    throw new Error('Failed to unlike comment');
-  }
-
-  console.log('Comment unliked successfully');
-  return response.json();
-};
