f03d3ac983af733a793e711faac8c1628fc113c1
이승현 <lsh746527@gmail.com>
2024-08-30 13:45:39 +0900

    refactor : 로딩 스피너 gif 파일 교체

--- components/animations/LoadingSpinner.tsx
+++ components/animations/LoadingSpinner.tsx
@@ -2,7 +2,7 @@ export default function LoadingSpinner() {
   return (
     <div className="flex items-center justify-center">
       <img
-        src="/assets/gif/loading-gif.gif" 
+        src="/assets/gif/loading.gif" 
         alt="Loading..."
         className="h-12 w-12" 
       />
