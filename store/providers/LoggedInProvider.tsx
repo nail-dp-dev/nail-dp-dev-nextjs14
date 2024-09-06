be35f2275cd68a562d8f2047be76f70aaf2060ce
Ju-jh <jjdong9108@gmail.com>
2024-09-06 15:28:23 +0900

    refactor : signup 시에 성공하면 login 되는부분 수정완료., refactor : login modal에서 로그인시 이전 로그인 알림 부분 수정 완료. #67

--- store/providers/LoggedInProvider.tsx
+++ store/providers/LoggedInProvider.tsx
@@ -1,26 +1,37 @@
 'use client';
 
-import React, { useEffect } from 'react';
+import React, { useEffect, useCallback } from 'react';
 import { useDispatch } from 'react-redux';
 import { getCookieValid } from '../../api/auth/validation/getCookieValid';
 import { getCookie } from '../../lib/getCookie';
-import { logOut } from '../slices/loginSlice';
+import { logIn, logOut } from '../slices/loginSlice';
+import { usePathname } from 'next/navigation';
 
 const LoggedInProvider = ({ children }: { children: React.ReactNode }) => {
   const dispatch = useDispatch();
+  const pathname = usePathname();
 
-  const checkCookie = async () => {
+  const checkCookie = useCallback(async () => {
     const cookie = getCookie('Authorization');
     if (cookie) {
       await getCookieValid(dispatch);
+      dispatch(logIn());
     } else {
       dispatch(logOut());
     }
-  };
+  }, [dispatch]);
 
   useEffect(() => {
+    // 최초 로드 시 쿠키 확인
     checkCookie();
-  }, []);
+  }, [checkCookie]);
+  
+  useEffect(() => {
+    // URL이 '/'일 때 쿠키 확인
+    if (pathname === '/') {
+      checkCookie();
+    }
+  }, [pathname, checkCookie]);
 
   return <>{children}</>;
 };
