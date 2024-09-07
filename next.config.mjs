dc745cb0bb94112d9d97ced30beb617b9dd65e93
이승현 <lsh746527@gmail.com>
2024-09-07 15:45:29 +0900

    chore : protocol 추가

--- next.config.mjs
+++ next.config.mjs
@@ -64,6 +64,10 @@ const nextConfig = {
         hostname: 'lh3.googleusercontent.com',
       },
       {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
         protocol: 'http',
         hostname: 'img1.kakaocdn.net',
       },
