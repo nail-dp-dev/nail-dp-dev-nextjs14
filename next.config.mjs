83aef87e21869122e503a4ad41b2af36e62ac79c
Ju-jh <jjdong9108@gmail.com>
2024-09-09 14:33:56 +0900

    fix : 내 아카이브 부분 특정 아카이브 부분 해결 완료., 특정 내 아카이브 의 좋아요 누른 게시글 부분 오류 확인 #109

--- next.config.mjs
+++ next.config.mjs
@@ -59,6 +59,14 @@ const nextConfig = {
         protocol: 'https',
         hostname: 'lh3.googleusercontent.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+      },
     ],
   },
 };
