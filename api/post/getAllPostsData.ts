032c6d08ada7c01106538a4fdc73bab9f7d09e93
LKW9 <92284361+LKW9@users.noreply.github.com>
2024-09-12 14:46:32 +0900

    Update getAllPostsData.ts

--- api/post/getAllPostsData.ts
+++ api/post/getAllPostsData.ts
@@ -4,7 +4,7 @@ export const getAllPostsData = async ({ category, size, cursorId }: PostsDataPro
 
   try {
 
-    let url = `http://127.0.0.1:8080/home?choice=${category}`
+    let url = `http://172.18.0.3:8080/home?choice=${category}`
     
     if (size) {
       url += `&size=${size}`
