1a9535c80cfd3abcce97819bec5269cfaec02a4b
LKW9 <92284361+LKW9@users.noreply.github.com>
2024-09-12 14:11:52 +0900

    Update getAllPostsData.ts

--- api/post/getAllPostsData.ts
+++ api/post/getAllPostsData.ts
@@ -4,7 +4,7 @@ export const getAllPostsData = async ({ category, size, cursorId }: PostsDataPro
 
   try {
 
-    let url = `${process.env.NEXT_PUBLIC_API_URL}/home?choice=${category}`
+    let url = `http://3.36.54.132:8080/home?choice=${category}`
     
     if (size) {
       url += `&size=${size}`
