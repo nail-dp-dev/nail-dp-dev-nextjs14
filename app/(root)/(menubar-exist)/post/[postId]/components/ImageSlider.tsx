b7e0b92d6e964f7b605269985125af5514ba2c14
이승현 <lsh746527@gmail.com>
2024-09-19 17:09:11 +0900

    chore : 동영상 재생 컨트롤러 추가 #83

--- app/(root)/(menubar-exist)/post/[postId]/components/ImageSlider.tsx
+++ app/(root)/(menubar-exist)/post/[postId]/components/ImageSlider.tsx
@@ -1,7 +1,6 @@
 'use client';
 
-import React, { useState, useEffect } from 'react';
-import Video from '../../../../../../components/ui/Video';
+import React, { useState, useEffect, useRef } from 'react';
 import ArrowLeft from '../../../../../../public/assets/svg/arrow-left.svg';
 import ArrowRight from '../../../../../../public/assets/svg/arrow-right.svg';
 
@@ -16,16 +15,20 @@ interface ImageSliderProps {
   onImageChange?: (url: string) => void;
 }
 
-// 이미지 슬라이드
 export default function ImageSlider({
   files,
   onImageChange,
 }: ImageSliderProps) {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const videoRef = useRef<HTMLVideoElement | null>(null);
 
   useEffect(() => {
     if (onImageChange) {
       onImageChange(files[currentIndex].fileUrl);
+    }
+
+    if (files[currentIndex].isVideo && videoRef.current) {
+      videoRef.current.play();
     }
   }, [currentIndex, files, onImageChange]);
 
@@ -43,7 +46,7 @@ export default function ImageSlider({
 
   return (
     <div className="relative h-full w-full">
-      <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center overflow-hidden rounded-2xl ">
+      <div className="absolute left-0 top-0 flex h-full w-full items-center justify-center overflow-hidden rounded-2xl">
         {files[currentIndex].isPhoto ? (
           <img
             src={files[currentIndex].fileUrl}
@@ -51,30 +54,59 @@ export default function ImageSlider({
             className="h-full w-full object-cover"
           />
         ) : (
-          <Video src={files[currentIndex].fileUrl} width="100%" height="100%" />
+          <video
+            ref={videoRef}
+            src={files[currentIndex].fileUrl}
+            className="h-full w-full object-cover"
+            width="100%"
+            height="100%"
+            controls
+            style={{ zIndex: 10 }}
+            controlsList="nodownload"
+          />
         )}
       </div>
+
+      {/* 좌우 이동 버튼 */}
       {files.length > 1 && (
         <>
-          <div className="absolute left-0 top-0 flex h-full w-full items-center justify-between">
-            <button onClick={goToPrevious} className="p-2 py-8 pr-8 text-white">
+          <div
+            className="absolute left-0 top-0 z-20 flex h-full w-full 
+            items-center justify-between"
+            style={{ pointerEvents: 'none' }}
+          >
+            <button
+              onClick={goToPrevious}
+              className="z-30 p-2 py-8 pr-8 text-white"
+              style={{ pointerEvents: 'auto' }}
+            >
               <ArrowLeft />
             </button>
-            <button onClick={goToNext} className="p-2 py-8 pl-8 text-white">
+            <button
+              onClick={goToNext}
+              className="z-30 p-2 py-8 pl-8 text-white"
+              style={{ pointerEvents: 'auto' }}
+            >
               <ArrowRight />
             </button>
           </div>
         </>
       )}
+
+      {/* 페이지 인디케이터 */}
       {files.length > 1 && (
-        <div className="absolute bottom-0 left-0 flex w-full items-center justify-center p-2">
+        <div
+          className="absolute bottom-0 left-0 z-20 
+          flex w-full items-center justify-center p-3"
+          style={{ pointerEvents: 'none' }}
+        >
           {files.map((_, index) => (
             <div
               key={index}
-              className={`mx-2 h-2 w-2 cursor-pointer rounded-full ${
-                index === currentIndex ? 'bg-darkPurple' : 'bg-addFolderGray'
-              }`}
+              className={`mx-2 h-2 w-2 cursor-pointer rounded-full 
+              ${index === currentIndex ? 'bg-darkPurple' : 'bg-addFolderGray'}`}
               onClick={() => setCurrentIndex(index)}
+              style={{ pointerEvents: 'auto' }}
             ></div>
           ))}
         </div>
